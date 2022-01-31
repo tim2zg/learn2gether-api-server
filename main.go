daa53f683206189d92c8663f0a49d2a39d0de70b
Your Name <ccu.schaller@outlook.com>
2022-01-31 19:10:50 +0100

    added json endpoint

--- main.go
+++ main.go
@@ -3,23 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
-
 	"github.com/valyala/fasthttp"
+	"log"
 )
 
 var (
-	addr     = "0.0.0.0:8080"
-	compress = flag.Bool("compress", false, "Whether to enable transparent response compression")
+	addr = "0.0.0.0:42069"
 )
 
 func main() {
 	flag.Parse()
 
 	h := requestHandler
-	if *compress {
-		h = fasthttp.CompressHandler(h)
-	}
 
 	if err := fasthttp.ListenAndServe(addr, h); err != nil {
 		log.Fatalf("Error in ListenAndServe: %s", err)
@@ -27,29 +22,37 @@ func main() {
 }
 
 func requestHandler(ctx *fasthttp.RequestCtx) {
-	fmt.Fprintf(ctx, "Hello, world!\n\n")
+	path := string(ctx.Path()[:])
+	if path == "/topics" {
+		fmt.Fprintf(ctx, "{\"id\":1035,\"uid\":\"3ebeaa4b-0307-4465-8534-df2d5b2eddb0\",\"valid_card\":\"36227206271667\",\"token\":\"tok_mastercard_prepaid\",\"invalid_card\":\"4000000000000341\",\"month\":\"10\",\"year\":\"2023\",\"ccv\":\"832\",\"ccv_amex\":\"9730\"}")
+		ctx.SetContentType("application/json; charset=utf8")
 
-	fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
-	fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
-	fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
-	fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
-	fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
-	fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
-	fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
-	fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
-	fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
-	fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
+	} else if path == "/home" {
+		fmt.Fprintf(ctx, "Hello, world!\n\n")
 
-	fmt.Fprintf(ctx, "Raw request is:\n---CUT---\n%s\n---CUT---", &ctx.Request)
+		fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
+		fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
+		fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
+		fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
+		fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
+		fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
+		fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
+		fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
+		fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
+		fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
 
-	ctx.SetContentType("text/plain; charset=utf8")
+		fmt.Fprintf(ctx, "Raw request is:\n---CUT---\n%s\n---CUT---", &ctx.Request)
 
-	// Set arbitrary headers
-	ctx.Response.Header.Set("X-My-Header", "my-header-value")
+		ctx.SetContentType("text/plain; charset=utf8")
 
-	// Set cookies
-	var c fasthttp.Cookie
-	c.SetKey("cookie-name")
-	c.SetValue("cookie-value")
-	ctx.Response.Header.SetCookie(&c)
+		// Set arbitrary headers
+		ctx.Response.Header.Set("X-My-Header", "my-header-value")
+
+		// Set cookies
+		var c fasthttp.Cookie
+		c.SetKey("cookie-name")
+		c.SetValue("cookie-value")
+		ctx.Response.Header.SetCookie(&c)
+	}
+
 }
