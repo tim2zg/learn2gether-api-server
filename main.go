f4b33e6a1661a5f210af8f84a094c48dec530b64
Your Name <ccu.schaller@outlook.com>
2022-02-10 08:36:59 +0100

    added json input

--- main.go
+++ main.go
@@ -1,58 +1,75 @@
 package main
 
 import (
-	"flag"
+	"encoding/json"
 	"fmt"
 	"github.com/valyala/fasthttp"
+	"io/ioutil"
 	"log"
+	"os"
 )
 
 var (
 	addr = "0.0.0.0:42069"
 )
 
-func main() {
-	flag.Parse()
+var data map[string]interface{}
 
-	h := requestHandler
+func main() {
+	data = dataloader()
+	fmt.Println(data)
+	h := mainrequestHandler
 
 	if err := fasthttp.ListenAndServe(addr, h); err != nil {
 		log.Fatalf("Error in ListenAndServe: %s", err)
 	}
 }
 
-func requestHandler(ctx *fasthttp.RequestCtx) {
-	path := string(ctx.Path()[:])
-	if path == "/topics" {
-		fmt.Fprintf(ctx, "{\"id\":1035,\"uid\":\"3ebeaa4b-0307-4465-8534-df2d5b2eddb0\",\"valid_card\":\"36227206271667\",\"token\":\"tok_mastercard_prepaid\",\"invalid_card\":\"4000000000000341\",\"month\":\"10\",\"year\":\"2023\",\"ccv\":\"832\",\"ccv_amex\":\"9730\"}")
-		ctx.SetContentType("application/json; charset=utf8")
-
-	} else if path == "/home" {
-		fmt.Fprintf(ctx, "Hello, world!\n\n")
-
-		fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
-		fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
-		fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
-		fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
-		fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
-		fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
-		fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
-		fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
-		fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
-		fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
-
-		fmt.Fprintf(ctx, "Raw request is:\n---CUT---\n%s\n---CUT---", &ctx.Request)
-
-		ctx.SetContentType("text/plain; charset=utf8")
-
-		// Set arbitrary headers
-		ctx.Response.Header.Set("X-My-Header", "my-header-value")
-
-		// Set cookies
-		var c fasthttp.Cookie
-		c.SetKey("cookie-name")
-		c.SetValue("cookie-value")
-		ctx.Response.Header.SetCookie(&c)
+func dataloader() map[string]interface{} {
+	// Open our jsonFile
+	jsonFile, err := os.Open("data.json")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
 	}
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer func(jsonFile *os.File) {
+		err := jsonFile.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(jsonFile)
 
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var result map[string]interface{}
+	err2 := json.Unmarshal([]byte(byteValue), &result)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	return result
+}
+
+func mainrequestHandler(rqu *fasthttp.RequestCtx) {
+	path := string(rqu.Path()[:])
+	switch path {
+	case "/topics":
+		j, err := json.Marshal(data)
+		if err != nil {
+			fmt.Printf("Error: %s", err.Error())
+		} else {
+			fmt.Println(string(j))
+		}
+		_, err2 := fmt.Fprintf(rqu, string(j))
+		if err2 != nil {
+			return
+		}
+		rqu.SetContentType("application/json; charset=utf8")
+	default:
+		rqu.SetContentType("text/plain")
+		rqu.SetStatusCode(404)
+		rqu.SetBodyString("404")
+
+	}
+	rqu.Response.Header.Set("Access-Control-Allow-Origin", "*")
 }
